60060685c680aa5eb4e770e0292d80787c9f11d8
Ahmad Izzan Zahrial <izzanzahrial@outlook.com>
2021-09-17 21:28:17 +0700

    add comment

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 // go get [name of the library, example: github.com/izzanzahrial/learn-go-module] <-- to get the module
 // to upgrade version of the library you can change directly in go.mod or use go get again
 // if there is a major upgrade, the solution is to change or create new module, rather than upgrade the version of the module
+// check go.mod for major update
 
 import (
 	"fmt"
