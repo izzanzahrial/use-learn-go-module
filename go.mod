aae8b9246e65babcf83e189d9605ecda87638f94
Ahmad Izzan Zahrial <izzanzahrial@outlook.com>
2021-09-17 21:26:55 +0700

    initial go module

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/izzanzahrial/use-learn-go-module
+
+go 1.17
+
+// you can the version to upgrade library then use go get
+
+// delete this part for major change
+// require github.com/izzanzahrial/learn-go-module v1.0.1
+// use go get [name of the library the updated version]
+// example go get github.com/izzanzahrial/learn-go-module/v2 <-- v2 is the major updated version
+
+// 
+require github.com/izzanzahrial/learn-go-module/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// go mod init [link of the repository, example: github.com/izzanzahrial/use-learn-go-module] <-- to initialize module
+// go get [name of the library, example: github.com/izzanzahrial/learn-go-module] <-- to get the module
+// to upgrade version of the library you can change directly in go.mod or use go get again
+// if there is a major upgrade, the solution is to change or create new module, rather than upgrade the version of the module
+
+import (
+	"fmt"
+
+	learn_go_module "github.com/izzanzahrial/learn-go-module/v2"
+)
+
+func main() {
+	fmt.Println(learn_go_module.TestGoModule("izzan"))
+}
